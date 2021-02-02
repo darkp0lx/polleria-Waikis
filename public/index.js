68bb1a293f57be2221f9d2153be732b285a02db2
hector <polbarazorda@gmail.com>
2021-02-01 23:34:42 -0500

    final responsive 320

---
+++ public/index.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+window.addEventListener("scroll",function(){
+  var header=document.querySelector(".Header");
+  header.classList.toggle("sticky",window.scrollY>0);
+})
